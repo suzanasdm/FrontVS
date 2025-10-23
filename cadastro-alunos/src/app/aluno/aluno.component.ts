2fcf240ad92b96b3320c3d8d85b3ab3979fb66e3
60002070 <suzanamor50607@gmail.com>
2025-10-23 20:54:13 -0300

    23/10

--- cadastro-alunos/src/app/aluno/aluno.component.ts
+++ cadastro-alunos/src/app/aluno/aluno.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule, NgForOf } from "../../../node_modules/@angular/common/common_module.d-NEF7UaHr";
+import { FormsModule } from '@angular/forms';
+import { ALuno } from '../models/aluno.model';
+import { AlunoService } from '../services/aluno.service';
 
 @Component({
   selector: 'app-aluno',
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './aluno.component.html',
   styleUrl: './aluno.component.css'
 })
 export class AlunoComponent {
+novoAluno: ALuno = {
+  ra: '',
+  nome:'',
+  DtNascimento: '',
+  curso: ''
+};
+listaAlunos: ALuno []= [];
 
+constructor (private alunoservice: AlunoService){
+  this.listaAlunos = this.alunoservice.getAlunos();
+}
+adicionarAluno(){
+  this.alunoservice.adicionarAluno({...this.novoAluno});
+  this.novoAluno = {ra: '',nome: '',DtNascimento:'',curso:''};
+}
 }
--- cadastro-alunos/src/app/app.component.ts
+++ cadastro-alunos/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { AlunoComponent } from './aluno/aluno.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ AlunoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
